b38f5f3af1f1dddbd6cfafed0efcadeb3ca2d661
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2019-05-10 13:59:22 -0400

    completed MVP

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,8 +7,7 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 function consume(a, b, cb) {
-
-  return cb(a, b);
+  return console.log(cb(a, b));
 }
 
 /* Step 2: Create several functions to callback with consume();
@@ -16,31 +15,33 @@ function consume(a, b, cb) {
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-
-function add(num1, num2, cb){
+function add(num1, num2){
   return (num1 + num2);
 }
 
-function multiply(num1, num2, cb){
+
+function multiply(num1, num2){
   return (num1 * num2);
 }
 
-function greeting(fName, lName, cb){
+function greeting(fName, lName){
   return (`Hello ${fName} ${lName}, nice to meet you!`);
 }
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
+console.log('check work')
+
 consume(2,2,add); // 4
 consume(10,16,multiply); // 160
 consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
-add(1, 1, function(sum){
-  console.log(sum);
-});
+// add(1, 1, function(sum){
+//   console.log(sum);
+// });
 
-multiply(3, 3, function(product){
-  console.log(product);
-});
+// multiply(3, 3, function(product){
+//   console.log(product);
+// });
 
 // ==== Closures ==== 
 
